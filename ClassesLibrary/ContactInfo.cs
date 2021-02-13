6291d1a32ff9c4b7db6b0c5d1d9987bd9d15030e
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-02-12 20:43:46 -0600

    Added ContactInfo class to classes library

---
+++ ClassesLibrary/ContactInfo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class ContactInfo
+    {
+        //Fields
+        private string _streetAddress;
+        private string _city;
+        private string _state;
+        private string _zip;
+        private string _phone;
+        private string _email;
+
+        //Properties
+        public string StreetAddress
+        {
+            get { return _streetAddress; }
+            set { _streetAddress = value; }
+        }
+
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+
+        public string State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+
+        public string Zip
+        {
+            get { return _zip; }
+            set { _zip = value; }
+        }
+
+        public string Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        //Constructors
+        public ContactInfo() { }
+
+        public ContactInfo(string streetAddress, string city, string state, string zip, string phone, string email)
+        {
+            StreetAddress = streetAddress;
+            City = city;
+            State = state;
+            Zip = zip;
+            Phone = phone;
+            Email = email;
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            return string.Format("Street: {0}\n" +
+                "City: {1}\n" +
+                "State: {2}\n" +
+                "Zip: {3}\n" +
+                "Phone: {4}\n" +
+                "Email: {5}\n",
+                StreetAddress, City, State, Zip, Phone, Email);
+        }//end ToString
+    }//end class
+}//end namespace
